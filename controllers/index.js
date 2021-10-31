194933005b40eb4580da96e8576b76b6a7c54c90
Sadie Sial <sadiesial@gmail.com>
2021-10-30 20:56:36 -0500

    displaying login/prfile pages now

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
 
-// API token + variables TO BE PROTECTED WITH .ENV FILE / work in progress heh
-apiKey = process.env.MURAL_API_KEY;
-var queryURL =
-  "https://data.cityofchicago.org/resource/we8h-apcf.json" + apiKey;
-var userSearch = "";
-var myData = undefined;
-
-
-handlerdata
+// // API token + variables TO BE PROTECTED WITH .ENV FILE / work in progress heh
+// apiKey = process.env.MURAL_API_KEY;
+// var queryURL =
+//   "https://data.cityofchicago.org/resource/we8h-apcf.json" + apiKey;
+// var userSearch = "";
+// var myData = undefined;
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
