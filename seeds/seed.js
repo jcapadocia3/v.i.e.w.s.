e2a234d41812451cb7e442c86d2fbf9923555868
Sadie Sial <sadiesial@gmail.com>
2021-10-31 01:14:54 -0500

    tester JSON added as comments to this file, as JSON files do not permit comments pfft

--- seeds/seed.js
+++ seeds/seed.js
@@ -23,3 +23,38 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
+
+
+// // BELOW THIS IS JSON, BUT!! JSON files do not allow comments. Placeholder area for muralData.json (currently projectData.json) for merge rzns
+// [
+//   {
+//     "mural_registration_id": "19304",
+//     "artist_credit": "Ouizi",
+//     "artwork_title": "West Town in bloom",
+//     "year_installed": "2019",
+//     "location_description": "1914 W. Chicago Ave"
+//   },
+//   {
+//     "mural_registration_id": "19239",
+//     "artist_credit": "Steven Teller",
+//     "artwork_title": "An Ode to Eleanor (Youth)",
+//     "year_installed": "2019",
+//     "street_address": "835 W Montrose Ave"
+//   },
+//   {
+//     "mural_registration_id": "19245",
+//     "artist_credit": "Eduardo Kobra",
+//     "artwork_title": "Vivian Maier",
+//     "media": "paint",
+//     "year_installed": "2017",
+//     "street_address": "1651-53 W North Ave"
+//   },
+//   {
+//     "mural_registration_id": "19150",
+//     "artist_credit": "Afrokilla",
+//     "media": "Paint",
+//     "year_installed": "2018",
+//     "location_description": "Cards Against Humanity HQ",
+//     "street_address": "1551 W Homer Street"
+//   }
+// ]
