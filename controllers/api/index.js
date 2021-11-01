a622fea474b9863efe51448ef3f477e9a465f618
Sadie Sial <sadiesial@gmail.com>
2021-10-31 22:49:00 -0500

    renamed to Projects to prevent insomnia bug

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const trailRoutes = require('./projectRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
