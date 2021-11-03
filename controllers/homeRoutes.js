7eebcfc5cfe9ef7d30f1ab9ef6200be6b1a09e63
James Capadocia <james.capadocia@gmail.com>
2021-11-02 23:40:13 -0500

    Created 'mural.hbs' and linked it to the proper route if called. Still need to create m'mural.js' file in /public to handle form submission and event listeners.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,33 @@ const withAuth = require('../utils/auth');
 
 const muralData = null;
 
+// Use withAuth middleware to prevent access to route
+
+router.get('/', (req, res) => {
+  console.log("it works!");
+  debugger;
+  res.render('login');
+});
+
+// router.get('/homepage', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Project }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('homepage', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.get('/home', async (req, res) => {
   try {
     // Get all murals and JOIN with user data
@@ -51,33 +78,6 @@ router.get('/mural/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// Use withAuth middleware to prevent access to route
-
-router.get('/', (req, res) => {
-  console.log("it works!");
-  debugger;
-  res.render('login');
-});
-
-// router.get('/homepage', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('homepage', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
