56d1e9237ba1b2d617660f62de6791b88accbca2
James Capadocia <james.capadocia@gmail.com>
2021-11-07 22:50:46 -0600

    Copy/Pasted previous styling changes into new branch after reverting the 'develop' branch.

--- public/js/mural.js
+++ public/js/mural.js
@@ -27,149 +27,6 @@ document.querySelector(".commentSubmit").addEventListener("click", commentHandle
 
 
 
-
-
-
-
-
-
-// const ratingStars = [...document.getElementsByClassName("rating__star")];
-
-// function executeRating(stars) {
-
-//   const starClassActive = "rating__star fas fa-star";
-//   const starClassInactive = "rating__star far fa-star";
-//   const starsLength = stars.length;
-
-//   let i;
-
-//   stars.map((star) => {
-//     star.onclick = () => {
-
-//       i = stars.indexOf(star);
-
-//       if (star.className===starClassInactive) {
-//         for (i; i >= 0; --i) stars[i].className = starClassActive;
-//         console.log([i]);
-//       } else {
-//         for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
-//       }
-//     };
-//     console.log(i);
-//   });
-// }
-// executeRating(ratingStars);
-
-// document
-//   .querySelector('.reviewForm')
-//   .addEventListener('submit', reviewHandler);
-
-
-
-
-
-
-
-
-
-
-// const reviewHandler = async (event) => {
-//     console.log("CLICK!");
-//     event.preventDefault();
-//     // document.location.replace('/home');
-
-//     // Collect values from the login form
-//     const rating1 = document.querySelector('#rating1');
-//     const rating2 = document.querySelector('#rating2');
-//     const rating3 = document.querySelector('#rating3');
-//     const rating4 = document.querySelector('#rating4');
-//     const rating5 = document.querySelector('#rating5');
-//     const comment = document.querySelector('#commentInput').value.trim();
-
-//     if (rating1 && comment) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ rating1, comment }),
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-
-//       if (response.ok) {
-//         // If successful, redirect the browser to the profile page
-//         console.log("111");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     } else if (rating2 && comment) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ rating2, comment }),
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-
-//       if (response.ok) {
-//         // If successful, redirect the browser to the profile page
-//         console.log("222");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     } else if (rating3 && comment) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ rating3, comment }),
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-
-//       if (response.ok) {
-//         // If successful, redirect the browser to the profile page
-//         console.log("333");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     } else if (rating4 && comment) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ rating4, comment }),
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-
-//       if (response.ok) {
-//         // If successful, redirect the browser to the profile page
-//         console.log("444");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     } else if (rating5 && comment) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ rating5, comment }),
-//         headers: { 'Content-Type': 'application/json' },
-//       })
-
-//       if (response.ok) {
-//         // If successful, redirect the browser to the profile page
-//         console.log("555");
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   };
-
-// document
-//   .querySelector('.reviewForm')
-//   .addEventListener('submit', reviewHandler);
-
-
-
-
-
-
-
-
 // WEB API - AJAX call + functions to follow:
 // declare API variables:
 apiKey = "5ht1h6zfn446h3vl2t1a4n2p7";
@@ -223,16 +80,16 @@ $("#mural-search-button").on("click", function (e) {
       muralArray.push(data[i]);
 
       let makeMuralOutput =
-        "Mural Title: " +
+        `<span class="muralStuff">Mural Title: </span>` +
         data[i].artwork_title +
         "<br>" +
-        "Artist Name: " +
+        `<span class="muralStuff">Artist Name: </span>` +
         data[i].artist_credit +
         "<br>" +
-        "Installment Year: " +
+        `<span class="muralStuff">Installment Year: </span>` +
         data[i].year_installed +
         "<br>" +
-        "Address: " +
+        `<span class="muralStuff">Address: </span>` +
         data[i].street_address +
         " Chicago, IL           " +
         data[i].zip +
@@ -245,4 +102,3 @@ $("#mural-search-button").on("click", function (e) {
   // console.log(muralArray)
 });
 
-// module.exports = ?
