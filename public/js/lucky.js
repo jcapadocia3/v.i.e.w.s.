59f307d48030c571cb6437d8e4896ca70b78ed43
James Capadocia <james.capadocia@gmail.com>
2021-11-09 19:29:48 -0600

    Final adjustments.

--- public/js/lucky.js
+++ public/js/lucky.js
@@ -1,7 +1,7 @@
 const luckyBtnHandler = async (event) => {
   event.preventDefault();
   console.log("it clicked");
-  document.location.replace("/mural/1");
+  document.location.replace("/mural/2");
 };
 
 document
